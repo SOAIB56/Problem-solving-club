ca091939822deaa56d0fea8b8bcd4f89862b2b23
Soaib Ahmed <129555727+SOAIB56@users.noreply.github.com>
2023-12-18 12:39:58 +0600

    Add files via upload

---
+++ extra contest/day 1/A_Parliament.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        /* code */
+        int n, m;
+        cin>>n>>m;
+        if(m*2>=n)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;;
+    }
+    
+    return 0;
+}
